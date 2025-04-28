833288bb30939f0ab1c1a269adccc0e83a8aa66d
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-28 23:00:19 +0400

    for debugging

--- account/script.ts
+++ account/script.ts
@@ -1,6 +1,6 @@
 const apiKeyElement = document.getElementById("api-key") as HTMLInputElement;
 if (apiKeyElement) {
-    const apiKey = localStorage.getItem("apiKey") || "";
+    const apiKey = localStorage.getItem("apiKey") || "no api key found :(";
     apiKeyElement.innerHTML = apiKey;
     
 }
