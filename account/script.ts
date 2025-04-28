a1983fcca1ae1fca18f719544eacd7d0416b836f
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-28 22:56:32 +0400

    fix: minor error in api.html

--- account/script.ts
+++ account/script.ts
@@ -1,15 +1,15 @@
-function displayAPIkey() {
-    const apiKey = localStorage.getItem('apiKey');
-    const apiKeyElement = document.getElementById('apiKey');
-    const apiKeyButton = document.getElementById('apiKeyButton');
+
     
-    if (apiKeyElement) {
-        apiKeyElement.textContent = apiKey || 'Please login to continue';
-        if (apiKeyButton) {
+const apiKeyButton = document.getElementById('apiKeyButton');
+if (apiKeyButton) {
+    apiKeyButton.addEventListener('click', () => {
+        const apiKey = localStorage.getItem('apiKey');
+        const apiKeyElement = document.getElementById('apiKey');
+        if (apiKeyElement) {
+            apiKeyElement.textContent = apiKey || 'Please login to continue';
             apiKeyButton.style.display = 'none';
-    }
-}
-    else {
-        console.error('API key element not found');
-    }
+        } else {
+            console.error('API key element not found');
+        }
+    });
 }
