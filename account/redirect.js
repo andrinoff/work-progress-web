29dc9941c9dedd095b8d68c9b61786887e6ec42b
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-28 14:02:31 +0400

    added handler for sign up

--- account/redirect.js
+++ account/redirect.js
@@ -27,3 +27,31 @@ signinform.addEventListener('submit', function (event) {
                     throw new Error('Failed to fetch API key');
                 }
             })});
+
+
+signupform.addEventListener('submit', function (event) {
+    event.preventDefault();
+    const email = document.getElementById('email-input').value;
+    const password = document.getElementById('password-input').value;
+// TODO add the site
+
+    const apiKey = fetch("https://work-progress-backend.vercel.app/api/server", {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json'
+                },
+                body: JSON.stringify({
+                    email: email,
+                    password: password,
+                    sign: "up"
+                })
+            }).then(response => {
+
+                if (response.ok) {
+                    localStorage.setItem('apiKey', apiKey);
+                    window.location.href = "api.html";
+                    return response.json();
+                } else {
+                    throw new Error('Failed to fetch API key');
+                }
+            })});
