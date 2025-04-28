94563cb145596396ae2c6fd2caac087a917438ca
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-28 23:17:37 +0400

    debug: debugging apiKey

--- account/script.ts
+++ account/script.ts
@@ -1,4 +1,5 @@
 const apiKeyElement = document.getElementById("api-key") as HTMLInputElement;
+console.log(localStorage.getItem("apiKey"))
 if (apiKeyElement) {
     const apiKey = localStorage.getItem("apiKey") || "no api key found :(";
     apiKeyElement.innerHTML = apiKey;
