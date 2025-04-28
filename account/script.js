f4bf2f50e0a76ebc2aab08f73e1515ff901841c0
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-28 23:28:45 +0400

    check last commit

--- account/script.js
+++ account/script.js
@@ -2,7 +2,7 @@ const apiKeyElement = document.getElementById("apiKey");
 console.log(localStorage.getItem("apiKey"))
 if (apiKeyElement) {
     const apiKey = localStorage.getItem("apiKey") || "no api key found :(";
-    apiKeyElement.textContent = apiKey;
+    apiKeyElement.innerHTML = apiKey;
     
 }
 alert("YOUR API KEY: " + localStorage.getItem("apiKey"));
