00afa79ad598c944d1c60fa580d4c564897e027f
Andrey Smirnoff <andrinoff.69@gmail.com>
2025-04-29 11:53:17 +0400

    fix: added a middle page before the api.html

--- account/redirect.js
+++ account/redirect.js
@@ -135,33 +135,3 @@ if (!signupform && window.location.pathname.includes('signup.html')) {
 
 
 
-// --- GitHub Callback Handler ---
-const urlParams = new URLSearchParams(window.location.search);
-const code = urlParams.get('code');
-if (code) {
-    // If the code is present, make a POST request to your backend to exchange it for an API key
-    fetch("https://work-progress-backend.vercel.app/api/server", {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({
-            code: code,
-            sign: "github" // Indicate this is a GitHub sign-in
-        })
-    })
-    .then(response => response.json())
-    .then(data => {
-        if (data && data.apiKey) {
-            localStorage.setItem('apiKey', data.apiKey);
-            window.location.href = "api.html";
-        } else {
-            console.error('GitHub sign-in failed:', data);
-            alert('GitHub sign-in failed. '+ data.message);
-        }
-    })
-    .catch(error => {
-        console.error('Error during GitHub sign-in:', error);
-        alert('An error occurred during GitHub sign-in. Please try again.');
-    });
-}
